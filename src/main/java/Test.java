08e53824a9ede7f3872def39d52ddd283692d4e0
TestLiu0724 <git_test_124@163.com>
2020-07-25 12:54:40 +0800

    修改

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,5 +5,8 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("jsfsljlsjglsk");
+        System.out.println("jsfsljlsjglsk");
+        System.out.println("jsfsljlsjglsk");
+        System.out.println("jsfsljlsjglsk");
     }
 }
